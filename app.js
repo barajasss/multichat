3adf0128a4dda1030a8f4374c304c86ef3447e96
Baraja Swargiary <barajaswargiary4444@gmail.com>
2021-07-18 14:58:12 +0530

    added packages

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 const express = require('express')
+const app = express()
+const { Server } = require('socket.io')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const PORT = process.env.PORT || 3000
+
+app.set('views', 'views')
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.listen(PORT, () => console.log('socket server listening at port ' + PORT))
+
